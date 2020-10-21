c900d1e907e7e3e9abaa66e00c54763cb93286bd
Lokarin <onukiamaral2@gmail.com>
2020-10-21 15:47:52 -0300

    Adicionei o Tor ao navegadores.sh

--- navegadores.sh
+++ navegadores.sh
@@ -3,7 +3,7 @@
 # Import the colors
 . "${HOME}/.cache/wal/colors.sh"
 
-nav=$(echo -e "Brave\nFirefox\nCliqz\nQutebrowser\nVimb" | dmenu -i -c -l 25 -nb "$color0" -nf "$color15" -sb "$color1" -sf "$color15"  -p "Abrir Navegador...")
+nav=$(echo -e "Brave\nFirefox\nCliqz\nQutebrowser\nVimb\nTor" | dmenu -i -c -l 25 -nb "$color0" -nf "$color15" -sb "$color1" -sf "$color15"  -p "Abrir Navegador...")
 
 case $nav in
 	Cliqz) cliqz ;;
@@ -11,4 +11,5 @@ case $nav in
 	Firefox) firefox;;
 	Brave) brave;;
 	Vimb) vimb;;
+	Tor) torbrowser-launcher;;
 esac
