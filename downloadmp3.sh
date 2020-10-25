361db6dfa526e18d4351f1b32a65f706cf1c84d7
Lokarin <onukiamaral2@gmail.com>
2020-10-25 11:18:56 -0300

    Novo --output no downloadmp3

--- downloadmp3.sh
+++ downloadmp3.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-youtube-dl --extract-audio --add-metadata --audio-format mp3 $1
+youtube-dl --extract-audio --add-metadata --output '~/Música/TouhouGameplay/%(title)s.%(ext)s' --audio-format mp3 $1
