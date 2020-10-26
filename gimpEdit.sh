e7079ff6d9353236e458ce743028b7eff25df4ed
Lokarin <onukiamaral2@gmail.com>
2020-10-26 08:27:44 -0300

    gimpEdit.sh agora funciona como o dmenuColors.sh

--- gimpEdit.sh
+++ gimpEdit.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-sudo gimp ~/Documentos/GimpFiles/$( ls ~/Documentos/GimpFiles/ | dmenu -l 30 )
+# Import the colors
+. "${HOME}/.cache/wal/colors.sh"
 
+sudo gimp ~/Documentos/GimpFiles/$( ls ~/Documentos/GimpFiles/ | dmenu -i -c -l 30 -nb "$color0" -nf "$color15" -sb "$color1" -sf "$color15"  -p "$1" )
