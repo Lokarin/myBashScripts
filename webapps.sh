e1edb6cb5c99ff587e9e60c295ea50c6a178732d
Lokarin <onukiamaral@protonmail.com>
2021-01-11 14:26:42 -0300

    SSBs são abertos com o LibreWolf ao invés do Firefox [webapps.sh]

--- webapps.sh
+++ webapps.sh
@@ -5,13 +5,24 @@
 
 app=$(echo -e "ProtonMail\nGitHub\nYouTube\nStartPage\nReddit\nTwitch\nTwitter\nWhatsapp" | dmenu -i -c -l 25 -nb "$color0" -nf "$color15" -sb "$color1" -sf "$color15" -p "Abrir WebApp...")
 
+#case $app in
+#        ProtonMail) firefox -ssb https://mail.protonmail.com/inbox;;
+#        GitHub) firefox -ssb https://github.com;;
+#        YouTube) firefox -ssb https://youtube.com;;
+#        StartPage) firefox -ssb https://startpage.com;;
+#        Reddit) firefox -ssb https://www.reddit.com;;
+#        Twitch) firefox -ssb https://www.twitch.tv/;;
+#        Twitter) firefox -ssb https://twitter.com/home;;
+#        Whatsapp) firefox -ssb https://web.whatsapp.com/;;
+#esac
+
 case $app in
-        ProtonMail) firefox -ssb https://mail.protonmail.com/inbox;;
-        GitHub) firefox -ssb https://github.com;;
-        YouTube) firefox -ssb https://youtube.com;;
-        StartPage) firefox -ssb https://startpage.com;;
-        Reddit) firefox -ssb https://www.reddit.com;;
-        Twitch) firefox -ssb https://www.twitch.tv/;;
-        Twitter) firefox -ssb https://twitter.com/home;;
-        Whatsapp) firefox -ssb https://web.whatsapp.com/;;
+        ProtonMail) librewolf -ssb https://mail.protonmail.com/inbox;;
+        GitHub) librewolf -ssb https://github.com;;
+        YouTube) librewolf -ssb https://youtube.com;;
+        StartPage) librewolf -ssb https://startpage.com;;
+        Reddit) librewolf -ssb https://www.reddit.com;;
+        Twitch) librewolf -ssb https://www.twitch.tv/;;
+        Twitter) librewolf -ssb https://twitter.com/home;;
+        Whatsapp) librewolf -ssb https://web.whatsapp.com/;;
 esac
