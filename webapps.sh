1959a3b4542157db8be35d739ce1ed1f05aa7d59
Lokarin <onukiamaral@protonmail.com>
2021-01-18 14:55:07 -0300

    StartPage para DuckDuckGo em 'webapps.sh'

--- webapps.sh
+++ webapps.sh
@@ -3,13 +3,13 @@
 # Import the colors
 . "${HOME}/.cache/wal/colors.sh"
 
-app=$(echo -e "ProtonMail\nGitHub\nYouTube\nStartPage\nReddit\nTwitch\nTwitter\nWhatsapp" | dmenu -i -c -l 25 -nb "$color0" -nf "$color15" -sb "$color1" -sf "$color15" -p "Abrir WebApp...")
+app=$(echo -e "ProtonMail\nGitHub\nYouTube\nDuckDuckGo\nReddit\nTwitch\nTwitter\nWhatsapp" | dmenu -i -c -l 25 -nb "$color0" -nf "$color15" -sb "$color1" -sf "$color15" -p "Abrir WebApp...")
 
 #case $app in
 #        ProtonMail) firefox -ssb https://mail.protonmail.com/inbox;;
 #        GitHub) firefox -ssb https://github.com;;
 #        YouTube) firefox -ssb https://youtube.com;;
-#        StartPage) firefox -ssb https://startpage.com;;
+#        DuckDuckGo) firefox -ssb https://duckduckgo.com;;
 #        Reddit) firefox -ssb https://www.reddit.com;;
 #        Twitch) firefox -ssb https://www.twitch.tv/;;
 #        Twitter) firefox -ssb https://twitter.com/home;;
@@ -20,7 +20,7 @@ case $app in
         ProtonMail) librewolf -ssb https://mail.protonmail.com/inbox;;
         GitHub) librewolf -ssb https://github.com;;
         YouTube) librewolf -ssb https://youtube.com;;
-        StartPage) librewolf -ssb https://startpage.com;;
+        DuckDuckGo) librewolf -ssb https://duckduckgo.com;;
         Reddit) librewolf -ssb https://www.reddit.com;;
         Twitch) librewolf -ssb https://www.twitch.tv/;;
         Twitter) librewolf -ssb https://twitter.com/home;;
